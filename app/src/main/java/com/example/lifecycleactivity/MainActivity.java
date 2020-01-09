45912c9e9788505126e2efa26583b6ce8d6a09c8
Dinh Quang <quangddinh@gmail.com>
2020-01-09 19:31:20 +0700

    Intent

--- app/src/main/java/com/example/lifecycleactivity/MainActivity.java
+++ app/src/main/java/com/example/lifecycleactivity/MainActivity.java
@@ -2,16 +2,31 @@ package com.example.lifecycleactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("BBB","onCreate");
+
+        mBtn = findViewById(R.id.Btn);
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
